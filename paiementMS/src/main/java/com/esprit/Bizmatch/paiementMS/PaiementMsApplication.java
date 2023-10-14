411eca6da687df47df418ad97fcb83e97d714fd0
Eya MAllouki <100727922+eyamallouki@users.noreply.github.com>
2023-10-14 18:39:33 +0100

    Add files via upload

---
+++ paiementMS/src/main/java/com/esprit/Bizmatch/paiementMS/PaiementMsApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.Bizmatch.paiementMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiementMsApplication.class, args);
+	}
+
+}
